ab6b4c11825eade2ff134bb2b44b148e8347e6a5
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2024-07-22 14:37:21 +0100

    new code

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import GuestList from './state/GuestList'
 // import UserSearch from './state/UserSearch'
 // import EventComponent from './events/EventComponent'
-import Booking from './props/components/Booking'
+import Booking from './/components/Booking'
 
 function App() {
 
